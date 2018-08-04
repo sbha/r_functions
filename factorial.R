b5ffaaff9f05167c677933e482d4b616bfa23a9d
sbha <sbharty@gmail.com>
2018-08-04 11:01:32 -0500

    drag and drop

---
+++ factorial.R
@@ -0,0 +1,19 @@
+# test function
+
+fct <- function(n){
+  if (n == 1) n * 1
+  else n * fct(n-1)
+}
+
+# fct(5)
+
+
+fact <- function(n){
+  z = 1
+  if (n>1) z = n * fct(n-1)
+  return(z)
+}
+
+# fact(5)
+
+
